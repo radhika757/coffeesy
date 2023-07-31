0df58045eb35295d3702fdf0663c6718ff939c3b
[radhika] <[raoradhika2000@gmail.com]>
2023-07-31 16:31:40 +0530

    fuked up grid for subscription pages

--- client/src/Shop/SubscriptionPlans.jsx
+++ client/src/Shop/SubscriptionPlans.jsx
@@ -1,15 +1,62 @@
+import styles from "./SubscriptionPlans.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Swiper css
+import "swiper/css";
+// import 'swiper/css/bundle';
+import "swiper/css/grid";
+// import "swiper/css/pagination";
+import { Grid, Pagination,Navigation } from "swiper";
+ 
 const SubscriptionPlans = () => {
   return (
     <>
-      <h2>All subscription plans</h2>
-      <span>Show all of them in slider</span>
-      <ul>
-        <li>choose a coffee</li>
-        <li>Pick a flavour</li>
-        <li>(show selected options of above) 
-            <span>Quantity selection, frequency selection</span>
-        </li>
-      </ul>
+      {/* <div className={styles.body}> */}
+        <Swiper
+          slidesPerView={2}
+          grid={{
+            rows: 3, 
+            fill: "row",
+          }}
+          navigation={true}
+          spaceBetween={10}
+          pagination={{
+            type: "fraction",
+          }}
+          modules={[Grid, Pagination,Navigation]}
+          className={`mySwiper ${styles.swiper}`}
+          // className={styles.swiper}
+        >
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3> Coffee Dark</h3>
+          </SwiperSlide>
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3>Coffee caramel</h3>
+          </SwiperSlide>
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3>Coffee Oats</h3>
+          </SwiperSlide>
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3>Coffee Hazelnut</h3>
+          </SwiperSlide>
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3>Coffee Cinnamon</h3>
+          </SwiperSlide>
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3>Coffee Pepper</h3>
+          </SwiperSlide>
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3>Coffee powder</h3>
+          </SwiperSlide>
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3>Coffee beans</h3>
+          </SwiperSlide>
+          <SwiperSlide className={styles["swiper-slide"]}>
+            <h3>Coffee select</h3>
+          </SwiperSlide>
+        </Swiper>
+      
+      {/* </div> */}
     </>
   );
 };
