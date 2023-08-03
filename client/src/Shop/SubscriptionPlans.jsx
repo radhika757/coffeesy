18eddab99178d28e85dc32302c3c6d46efbdc91d
[radhika] <[raoradhika2000@gmail.com]>
2023-08-03 17:25:21 +0530

    Grid with slider readyyyy

--- client/src/Shop/SubscriptionPlans.jsx
+++ client/src/Shop/SubscriptionPlans.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
 import styles from "./SubscriptionPlans.module.css";
+import left from "../assets/subscribe/left.png";
+import right from "../assets/subscribe/right.png";
 
 function SubscriptionPlans() {
   const [currentSlide, setCurrentSlide] = useState(1);
+  const [selectedState, setSelectedSate] = useState(false);
 
   const handleNextSlide = () => {
     setCurrentSlide((prev) => prev + 1);
@@ -16,23 +19,34 @@ function SubscriptionPlans() {
       case 1:
         return (
           <>
-            {/* // <div className={styles['slide-box']}> */}
-            <div className={styles["slide-box-headers"]}>
-              <span></span>
-              <h2>Choose a Coffee</h2>
-              <button onClick={handleNextSlide}>Pick a Flavour > </button>
+            <div className={styles["main-container-box"]}>
+              <div className={styles["slide-box"]}>
+                <div className={styles["slide-box-headers"]}>
+                  <button onClick={handlePreviousSlide} disabled>
+                    <span> Choose a Coffee</span>
+                    <img src={left} style={{ margin: "5px" }} />
+                  </button>
+                </div>
+                <div className={styles.title}>
+                  <h2>Choose a Coffee</h2>
+                </div>
+                <div className={styles.right}>
+                  <button onClick={handleNextSlide}>
+                  <img src={right} style={{ margin: "5px" }} />
+                    <span>Pick a Flavour</span>
+                
+                  </button>
+                </div>
+              </div>
+              <div className={styles["options-grid"]}>
+                <div className={styles["grid-item"]}>Instant Coffee</div>
+                <div className={styles["grid-item"]}>Cold brew</div>
+                <div className={styles["grid-item"]}>Hot brew</div>
+                <div className={styles["grid-item"]}>Ground coffee</div>
+                <div className={styles["grid-item"]}>Cold coffee cans</div>
+                <div className={styles["grid-item"]}>Iced coffee bottles</div>
+              </div>
             </div>
-
-            <div className={styles.options}>
-              <span>Instant Coffee</span>
-              <span>Cold brew</span>
-              <span>Hot brew</span>
-              <span>Ground coffee</span>
-              <span>Cold coffee cans</span>
-              <span>Iced coffee bottles</span>
-            </div>
-
-            {/* // </div> */}
           </>
         );
       case 2:
